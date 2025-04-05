8852678b559d833506a3ad670b73aa3c8445bbc1
anand_23 <155795811+Tech-Nomadic-X@users.noreply.github.com>
2025-04-05 13:28:36 +0530

    Delete Q9.R

--- Q9.R
+++
@@ -1,7 +0,0 @@
-# 9. Write R code that concatenates the strings "Data", "Science", and "R" into a single string with hyphens (-) as
-#    separators.
-
-cat("Data", "Science", "R", sep = "-")
-
-# Output:
-# Data-Science-R
